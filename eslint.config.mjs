46ff99e136f7e4d55771d549d57c8f3e86b9ad34
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2025-08-24 22:47:57 +0900

    feat: 사용하지 않는 상수 관련 eslint 속성 비활성화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+//@typescript-eslint/no-unused-vars
+// 비활성화 하려면 어떻게 해야하는지 알려줘
+// 코파일럿아 너가 알려줘
+// https://stackoverflow.com/questions/69882248/how-to-disable-eslint-rules-in-eslint-config-js-with-eslint-8
+// https://eslint.org/docs/latest/use/configure/configuration-files#disabling-rules-with
+//
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
@@ -19,6 +26,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
 ];
 
